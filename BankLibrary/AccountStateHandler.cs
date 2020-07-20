b01ff0bc052c2c5f77715b182654dca8f0d69ac4
Pakollya <52146842+Pakollya@users.noreply.github.com>
2020-07-20 09:10:29 +0300

    bank

---
+++ BankLibrary/AccountStateHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLibrary
+{
+    public delegate void AccountStateHandler(object sendler, AccountEventArgs e);
+
+    public class AccountEventArgs
+    {
+        public string Message { get; private set; }
+
+        public decimal Summ { get; private set; }
+
+        public AccountEventArgs(string _mes, decimal _sum)
+        {
+            Message = _mes;
+            Summ = _sum;
+        }
+    }
+}
